7de1ce37e176827d335e6e06640d0b4dd1760582
m.omar <m.omar@edraksoftware.com>
2020-05-11 10:06:56 +0200

    080 Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input, Output,EventEmitter} from '@angular/core';
 import { Recipe } from '../../recipe.model';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -7,10 +8,16 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 @Input() recipeProp :Recipe;
+@Output() outRecipeItem = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.recipeProp);
   }
 
+  onItemClicked(item : Recipe)
+  {
+    this.outRecipeItem.emit(item);
+  }  
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +6,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe('A Test Recipe','This is simply Description','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0_yeHO_Wv7q65HAMOHYwshnD4r47E6zdaJa2Hn2h0lUaseD_B&usqp=CAU'),
-                       new Recipe('A Test Recipe','This is simply Description','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0_yeHO_Wv7q65HAMOHYwshnD4r47E6zdaJa2Hn2h0lUaseD_B&usqp=CAU')];
+  recipes: Recipe[] = [new Recipe('A Test Recipe 1','This is simply Description 1','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0_yeHO_Wv7q65HAMOHYwshnD4r47E6zdaJa2Hn2h0lUaseD_B&usqp=CAU'),
+                       new Recipe('A Test Recipe 2','This is simply Description 2','https://previews.123rf.com/images/vmalafeevskiy/vmalafeevskiy1706/vmalafeevskiy170600007/80167889-fast-food-drawings-white-background.jpg')];
+  @Output() outSelectedItem = new EventEmitter<Recipe>();                   
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onItemClicked(item)
+  {
+    this.outSelectedItem.emit(item);
+  }
 }
